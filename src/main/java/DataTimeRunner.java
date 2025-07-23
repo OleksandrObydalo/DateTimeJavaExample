79fd517ec105312d0dd8f0a1880c7fe09302873a
OleksandrObydalo <oleksandr.obydalo@nure.ua>
2025-07-23 20:01:54 +0300

    Add Instant object and do operations with it

---
+++ src/main/java/DataTimeRunner.java
@@ -0,0 +1,18 @@
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+
+public class DataTimeRunner {
+    public static void main(String[] args) {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+        System.out.println(instant.toEpochMilli());
+        System.out.println(Instant.ofEpochSecond(0));
+        System.out.println(Instant.ofEpochMilli(12222230000l));
+
+        System.out.println(instant.atZone(ZoneId.of("Africa/Tunis")));
+
+        System.out.println(instant.plus(1, ChronoUnit.HOURS));
+        System.out.println(instant.plus(1, ChronoUnit.MONTHS)); //Causes Runtime error, as you can't add month to Instant
+    }
+}
